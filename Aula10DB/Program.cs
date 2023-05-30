3fbf54438bdea0fb9f70743d4e65753fd6f7289d
Gustavo Henrique de Moraes <104281604+GustavoMraes@users.noreply.github.com>
2023-05-30 17:41:32 -0300

    Fiz o program.cs mas falta o pedido

--- Aula10DB/Program.cs
+++ Aula10DB/Program.cs
@@ -1,2 +1,50 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+    using Microsoft.Data.Sqlite;
+using Aula10DB.Database;
+using Aula10DB.Repositories;
+using Aula10DB.Models;
+
+
+var databaseConfig = new DatabaseConfig();
+var databaseSetup = new DatabaseSetup(databaseConfig);
+
+var clienteRepository = new ClienteRepository(databaseConfig);
+
+var modelName = args[0];
+var modelAction = args[1];
+
+
+if(modelName == "Cliente")
+{
+    if(modelAction == "List")
+    {
+        Console.WriteLine("Cliente Listar");
+        foreach (var cliente in clienteRepository.GetAll())
+        {
+            Console.WriteLine($"{cliente.ClienteID}, {cliente.Endereco}, {cliente.Cidade}, {cliente.Regiao}, {cliente.CodigoPostal}, {cliente.Pais} ,{cliente.Telefone}");
+        }
+    }
+
+    if(modelAction == "New")
+    {
+        Console.WriteLine("Cliente Inserir");
+
+
+
+        var clienteID = Convert.ToInt32(args[2]);   
+        string endereco  = args[3];
+        string cidade  = args[4];
+        string regiao  = args[5];
+        string codigoPostal  = args[6];
+        string pais  = args[7];
+        string telefone  = args[8];
+       
+
+
+
+
+
+        var cliente = new Cliente(clienteID, endereco, cidade, regiao, codigoPostal, pais, telefone);
+        clienteRepository.Save(cliente);
+    }
+    
+}
