2678bab3f00694ca63af7915595163a9f3755eac
Gustavo Henrique de Moraes <104281604+GustavoMraes@users.noreply.github.com>
2023-05-30 17:14:37 -0300

    Agora tá certinho

--- Aula10DB/PedidoRepository.cs
+++ Aula10DB/PedidoRepository.cs
@@ -1,83 +1,89 @@
+
 using Aula10DB.Database;
 using Aula10DB.Models;
 using Microsoft.Data.Sqlite;
 namespace Aula10DB.Repositories;
-class ClienteRepository
+class PedidoRepository
 {
 private readonly DatabaseConfig _databaseConfig;
-public ClienteRepository(DatabaseConfig databaseConfig)
+
+public PedidoRepository (DatabaseConfig databaseConfig)
 {
 _databaseConfig = databaseConfig;
 }
 
-public List GetAll()
+public List<Pedido> GetAll()
 {
-var clientes = new List();
+    
+var pedidos = new List<Pedido>();
 
 
 var connection = new SqliteConnection(_databaseConfig.ConnectionString);
 connection.Open();
 
 var command = connection.CreateCommand();
-command.CommandText = "SELECT * FROM Cliente";
+command.CommandText = "SELECT * FROM Pedido";
 
 var reader = command.ExecuteReader();
 
 while(reader.Read())
 {
-var clienteid = reader.GetInt32(0);
-var endereco = reader.GetString(1);
-var cidade = reader.GetString(2);
-var regiao = reader.GetString(3);
-var codigopostal = reader.GetString(4);
-var pais = reader.GetString(5);
-var telefone = reader.GetString(6);
-var cliente = ReaderToCliente(reader);
-clientes.Add(cliente);
+var PedidoID = reader.GetInt32(0);
+var EmpregadoID = reader.GetString(1);
+var DataPedido = reader.GetString(2);
+var Peso = reader.GetInt32(3);
+var CodTransportadora = reader.GetString(4);
+var PedidoClienteId = reader.GetString(5);
+
+
+var pedido = ReaderToPedido(reader);
+
+
+pedidos.Add(pedido);
 }
 
 connection.Close();
 
-return clientes;
+return pedidos;
 }
 
-public Cliente Save(Cliente cliente)
+public Pedido Save(Pedido pedido)
 {
 var connection = new SqliteConnection(_databaseConfig.ConnectionString);
 connection.Open();
 
 var command = connection.CreateCommand();
-command.CommandText = "INSERT INTO Cliente VALUES($clienteid, $endereco, $cidade, $regiao, $codigopostal, $pais, $telefone)";
-command.Parameters.AddWithValue("$clienteid", cliente.ClienteId);
-command.Parameters.AddWithValue("$endereco", cliente.Enderenco);
-command.Parameters.AddWithValue("$cidade", cliente.Cidade);
-command.Parameters.AddWithValue("$regiao", cliente.Regiao);
-command.Parameters.AddWithValue("$codigopostal", cliente.CodigoPostal);
-command.Parameters.AddWithValue("$pais", cliente.Pais);
-command.Parameters.AddWithValue("$telefone", cliente.Telefone);
 
+
+command.CommandText = "INSERT INTO Pedido VALUES($id, $ram, $processor)";
+command.Parameters.AddWithValue("$PedidoID", pedido.PedidoID);
+command.Parameters.AddWithValue("$EmpregadoID", pedido.EmpregadoID);
+command.Parameters.AddWithValue("$DataPedido", pedido.DataPedido);
+command.Parameters.AddWithValue("$Peso", pedido.Peso);
+command.Parameters.AddWithValue("$CodTransportadora", pedido.CodTransportadora);
+command.Parameters.AddWithValue("$PedidoClienteId", pedido.PedidoClienteId);
 command.ExecuteNonQuery();
 connection.Close();
 
-return cliente;
+return pedido;
 }
-public Cliente GetById(int clienteid)
+public Pedido GetById(int id)
 {
 var connection = new SqliteConnection(_databaseConfig.ConnectionString);
 connection.Open();
 
 var command = connection.CreateCommand();
-command.CommandText = "SELECT * FROM Cliente WHERE (clienteid = $clienteid)";
-command.Parameters.AddWithValue("$clienteid", clienteid);
+command.CommandText = "SELECT * FROM Pedido WHERE (id = $id)";
+command.Parameters.AddWithValue("$id", id);
 
 var reader = command.ExecuteReader();
 reader.Read();
 
-var cliente = ReaderToCliente(reader);
+var pedido = ReaderToPedido(reader);
 
 connection.Close();
 
-return cliente;
+return pedido;
 }
 
 public bool ExitsById(int id)
@@ -86,18 +92,19 @@ var connection = new SqliteConnection(_databaseConfig.ConnectionString);
 connection.Open();
 
 var command = connection.CreateCommand();
-command.CommandText = "SELECT count(id) FROM Cliente WHERE (clienteid = $id)";
+command.CommandText = "SELECT count(id) FROM Pedido WHERE (id = $id)";
 command.Parameters.AddWithValue("$id", id);
-
 var reader = command.ExecuteReader();
 reader.Read();
 var result = reader.GetBoolean(0);
 
 return result;
 }
-private Cliente ReaderToCliente(SqliteDataReader reader)
-{
-var cliente = new Cliente(reader.GetInt32(0), reader.GetString(1), reader.GetString(2),reader.GetString(3),reader.GetString(4),reader.GetString(5), reader.GetString(6));
 
-return cliente;
+
+private Pedido ReaderToPedido(SqliteDataReader reader)
+{
+var pedido = new Pedido(reader.GetInt32(0), reader.GetInt32(1), reader.GetString(2), reader.GetString(3), reader.GetInt32(4), reader.GetInt32(5));
+return pedido;
+}
 }
