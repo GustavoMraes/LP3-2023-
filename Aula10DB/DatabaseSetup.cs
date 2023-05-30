2bcb2bfe0c8499f457a93be6a36050c46d6927db
Gustavo Lemos <75107497+lemos-guto@users.noreply.github.com>
2023-05-30 17:43:17 -0300

    fiz o setup pedido e clientela

--- Aula10DB/DatabaseSetup.cs
+++ Aula10DB/DatabaseSetup.cs
@@ -9,10 +9,11 @@ class DatabaseSetup
     public DatabaseSetup(DatabaseConfig databaseConfig)
     {
         _databaseConfig = databaseConfig;
-        CreateComputerTable();
+        CreateClienteTable();
+        CreatePedidoTable();
     }
 
-    private void CreateComputerTable()
+    private void CreateClienteTable()
     {
         
         var connection = new SqliteConnection(_databaseConfig.ConnectionString);
@@ -20,10 +21,36 @@ class DatabaseSetup
 
         var command = connection.CreateCommand();
         command.CommandText = @"
-            CREATE TABLE IF NOT EXISTS Computers(
-                id int not null primary key,
-                ram varchar(100) not null,
-                processor varchar(100) not null
+            CREATE TABLE IF NOT EXISTS Cliente(
+                clientid int not null primary key,
+                endereco varchar(100) not null,
+                cidade varchar(100) not null,
+                regiao varchar(100) not null,
+                codigopostal varchar(100) not null,
+                pais varchar(100) not null,
+                telefone varchar(100) not null
+            );
+        ";
+
+        command.ExecuteNonQuery();
+        connection.Close();
+    }
+        private void CreatePedidoTable()
+    {
+        
+        var connection = new SqliteConnection(_databaseConfig.ConnectionString);
+        connection.Open();
+
+        var command = connection.CreateCommand();
+        command.CommandText = @"
+            CREATE TABLE IF NOT EXISTS Pedido(
+                clientid int not null primary key,
+                endereco varchar(100) not null,
+                cidade varchar(100) not null,
+                regiao varchar(100) not null,
+                codigopostal varchar(100) not null,
+                pais varchar(100) not null,
+                telefone varchar(100) not null
             );
         ";
 
@@ -31,5 +58,3 @@ class DatabaseSetup
         connection.Close();
     }
 }
-
-
